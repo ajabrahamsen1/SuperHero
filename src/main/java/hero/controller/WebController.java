4c105c205389edee9682d12b864654510590cc0e
ajabrahamsen1 <46762854+ajabrahamsen1@users.noreply.github.com>
2019-04-17 21:23:32 -0500

    Updated WebController
    Removed accidental import.

--- src/main/java/hero/controller/WebController.java
+++ src/main/java/hero/controller/WebController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import dmacc.beans.House;
 import hero.beans.Profile;
 import hero.repository.ProfileRepository;
 
